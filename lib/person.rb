fe9e8314afd442677462a2a1d13eb32cc5099ee0
Brittany Alleman <brittany.alleman1@gmail.com>
2019-07-15 21:58:39 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -11,6 +11,11 @@ class Person
       @name = new_name
    end
 
+   
+   beyonce = Person.new
+   beyonce.name = "Beyonce aka Yonce aka Mrs Carter"
+
+
     def beyonce
       @beyonce = "Beyonce"
     end #we were missing an end here
@@ -23,3 +28,5 @@ class Person
 beyonce.job
 
     end
+    beyonce = Person.new
+    beyonce.name = "Beyonce aka Yonce aka Mrs Carter"
