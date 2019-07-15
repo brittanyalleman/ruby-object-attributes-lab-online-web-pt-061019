6a449bfe57b1c1b6b72b1298013faaf5ca49bae8
Brittany Alleman <brittany.alleman1@gmail.com>
2019-07-15 21:52:52 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -6,17 +6,20 @@ class Person
    def name
      @name
    end
+  
     def name=(new_name)
       @name = new_name
    end
 
     def beyonce
       @beyonce = "Beyonce"
-  
+    end #we were missing an end here
 
-  def job
-    job = "Singer"
-  end
-  end
+  	def job
+   	 job = "Singer"
+ 	 end
+  end#what is this end for?
+
 beyonce.job
+
     end
